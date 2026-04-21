380d3c1751b1432d58774ea5e60981bf19e4a459
RdTatimmussaadah <timitati22@gmail.com>
2026-04-21 13:30:15 +0700

    fix typo

--- js/riwayat.js
+++ js/riwayat.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
       renderSummaryStats();
       drawTrendCharts();
       renderHistoryBars();
-      renderHistoryBars();
+      // renderHistoryBars();
     });
 
     // onDataUpdate((data) => {
